8ec9b918b3e73f9e2b1a302325746045df6dc4e5
Naoumi1214 <wada121400@gmail.com>
2020-06-13 17:39:37 +0900

    update : mysqlの接続テストに成功した

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 const testValidator = require('../midleware/validators/testValidator')
 const {validationResult} = require('express-validator');
+const connection = require('../database/mysqlConnection');
 
 /* GET home page. */
 router.get('/', testValidator, (req, res, next) => {
-    // バリデーションの結果にエラーがあるかのチェック
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({errors: errors.array()});
-    }
+    connection.query('SELECT * FROM tests',(error, results, fields) =>{
+        if(error) throw error;
+        res.send(results);
+    });
 
-    res.send('b');
 });
+
+
 
 
 module.exports = router;
