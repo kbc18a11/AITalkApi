2b4136051f49ae76f6a402ad40cfc8b27599e16d
Naoumi1214 <wada121400@gmail.com>
2020-06-15 11:46:55 +0900

    update : all()の実装が完了した

--- model/AbstractModel.js
+++ model/AbstractModel.js
@@ -1,10 +1,33 @@
 const {validationResult} = require('express-validator');
 const connection = require('../database/mysqlConnection');
-const testValidator = require('../midleware/validators/testValidator')
+const testValidator = require('../midleware/validators/testValidator');
 
 class AbstractModel {
-    constructor(props) {
-
+    constructor() {
     }
 
+    /**
+     * テーブル名を取得する抽象メソッド
+     * @returns {null}
+     * @constructor
+     */
+    static get abstractTABLE_NAME() {
+    }
+
+    /**
+     * 対応したテーブルのレコードをすべて取得
+     * @returns {Promise<*>}
+     */
+    static async all() {
+        const sql = 'SELECT * FROM ??';
+        //const errorSql = 'SELECT * FROM ?';
+
+        //SQLを実行(rowsがSQLの実行結果)
+        const [rows, fields] = await connection.query(sql, [this.abstractTABLE_NAME]);
+
+        //console.log(rows);
+        return rows;
+    }
 }
+
+module.exports = AbstractModel;
