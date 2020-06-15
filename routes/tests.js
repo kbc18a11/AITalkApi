2dd9eb3ec49fc635bbbdfcd1807b7204e74f2e2c
Naoumi1214 <wada121400@gmail.com>
2020-06-15 11:23:33 +0900

    fix : index.js -> test.jsに名前変更

--- routes/tests.js
+++ routes/tests.js
@@ -3,17 +3,22 @@ const router = express.Router();
 const testValidator = require('../midleware/validators/testValidator')
 const {validationResult} = require('express-validator');
 const connection = require('../database/mysqlConnection');
+const Tests = require('../model/Tests');
 
 /* GET home page. */
 router.get('/', testValidator, (req, res, next) => {
-    connection.query('SELECT * FROM tests',(error, results, fields) =>{
-        if(error) throw error;
-        res.send(results);
+    let a = 'aaa';
+    /*const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }*/
+
+    Tests.all().then(data => {
+        console.log(data)
+        return res.send(data);
     });
 
 });
-
-
 
 
 module.exports = router;
