4df3a00fcd1d540d8a64ca45a1136ccaf73ed60a
Naoumi1214 <wada121400@gmail.com>
2020-06-15 11:57:59 +0900

    fix : 不要なrequireを削除した

--- model/AbstractModel.js
+++ model/AbstractModel.js
@@ -1,6 +1,5 @@
 const {validationResult} = require('express-validator');
 const connection = require('../database/mysqlConnection');
-const testValidator = require('../midleware/validators/testValidator');
 
 class AbstractModel {
     constructor() {
