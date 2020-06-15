a0aa1fa99a905af3ba807a93431f4c0c6c16c1b4
Naoumi1214 <wada121400@gmail.com>
2020-06-15 11:49:36 +0900

    update : RestのGETの例が完成した

--- routes/tests.js
+++ routes/tests.js
@@ -5,14 +5,24 @@ const {validationResult} = require('express-validator');
 const connection = require('../database/mysqlConnection');
 const Tests = require('../model/Tests');
 
-/* GET home page. */
+/**
+ * @GET
+ * testsのレコードをすべて取得
+ */
 router.get('/', testValidator, (req, res, next) => {
 
-    //データをすべて取得
-    Tests.all().then(data => {
-        console.log(data)
-        return res.send(data);
-    });
+    //レコードをすべて取得
+    Tests.all()
+        .then((data) => {
+            console.log(data)
+            //レコードを返す
+            return res.send(data);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500);
+            return res.send({'error': 'サーバー側でエラーが発生しました'});
+        });
 
 });
 
