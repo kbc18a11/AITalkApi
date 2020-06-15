511390e1e9883f2d13b360d9b9f939c74a12b6d5
Naoumi1214 <wada121400@gmail.com>
2020-06-15 11:29:06 +0900

    add : testsテーブルに対応したモデル

---
+++ model/Tests.js
@@ -0,0 +1,18 @@
+const  AbstractModel = require('./AbstractModel');
+
+class Tests extends AbstractModel{
+    constructor() {
+        super();
+    }
+
+    /**
+     * テーブル名
+     * @override
+     * @returns {string}
+     */
+    static get abstractTABLE_NAME(){
+        return 'tests';
+    }
+}
+
+module.exports = Tests;
