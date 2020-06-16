19775c31d64df142ecd24ea3f360b923e9643af5
Naoumi1214 <wada121400@gmail.com>
2020-06-16 14:58:16 +0900

    update : サンプル用のクラスのupdateメソッドの実装が完了した

--- model/Tests.js
+++ model/Tests.js
@@ -1,4 +1,6 @@
 const AbstractModel = require('./AbstractModel');
+const connection = require('../database/mysqlConnection');
+require('date-utils');
 
 class Tests extends AbstractModel {
     constructor() {
@@ -14,6 +16,16 @@ class Tests extends AbstractModel {
         return 'tests';
     }
 
+    static async update(insertParam) {
+        //UPDATE文
+        const sql = `UPDATE ${this.abstractTABLE_NAME} SET text = ?,updated_at = ? WHERE id = ?`;
+
+        //create_at用の日付時間取得
+        insertParam.updated_at = new Date().toFormat('YYYY-MM-DD HH:MI:SS');
+
+        //SQLを実行
+        await connection.query(sql, [insertParam.text, insertParam.updated_at, insertParam.id]);
+    }
 }
 
 module.exports = Tests;
