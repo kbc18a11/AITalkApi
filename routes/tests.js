82f3f7a3339694edc942a50fe21761bb92239431
Naoumi1214 <wada121400@gmail.com>
2020-06-16 11:35:06 +0900

    upate : GETとPOSTのサンプルを実装した

--- routes/tests.js
+++ routes/tests.js
@@ -5,27 +5,46 @@ const {validationResult} = require('express-validator');
 const connection = require('../database/mysqlConnection');
 const Tests = require('../model/Tests');
 
+
 /**
  * @GET
  * testsのレコードをすべて取得
  */
-router.get('/tests', (req, res, next) => {
+router.get('/tests', async (req, res, next) => {
 
-    //レコードをすべて取得
-    Tests.all()
-        .then((rows) => {
-            console.log(rows)
-            //レコードを返す
-            return res.send(rows);
-        })
-        .catch((error) => {
-            console.log(error);
-            res.status(500);
-            return res.send({'error': 'サーバー側でエラーが発生しました'});
-        });
+    try {
+        //レコードをすべて取得
+        const allRows = await Tests.all();
+        //console.log(allRows);
+
+        //レコードを返す
+        return res.send(allRows);
+    } catch (error) {
+        //レコードの取得失敗時
+        console.log(error);
+        res.status(500);
+        return res.send({'error': 'サーバー側でエラーが発生しました'});
+    }
 
 });
 
-router.post('/test');
+router.post('/test', testValidator.post, async (req, res, next) => {
+    //バリデーションの結果にエラーがあるかのチェック
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({errors: errors.array()});
+    }
+
+    try {
+        //データの挿入開始
+        await Tests.insert({text: req.query.text});
+        return res.send({'insertResult': true});
+    } catch (error) {
+        //レコードの挿入失敗時
+        console.log(error);
+        res.status(500);
+        return res.send({'insertResult': false});
+    }
+});
 
 module.exports = router;
