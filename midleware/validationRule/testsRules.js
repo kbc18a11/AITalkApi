fa8a0dab421b758127460ff89fc515a4e7aab02b
Naoumi1214 <wada121400@gmail.com>
2020-06-16 14:30:42 +0900

    update : エラーメッセージの実装を完了した

--- midleware/validationRule/testsRules.js
+++ midleware/validationRule/testsRules.js
@@ -1,10 +1,22 @@
 module.exports = {
     //post用
     post: {
-        text: 'required'
+        rule: {
+            text: 'required'
+        },
+        errorMessage: {
+            required: '必須項目です。',
+        }
     },
     //put用
-    put: [
-
-    ]
+    put: {
+        rule: {
+            id: 'required|integer',
+            text: 'required'
+        },
+        errorMessage: {
+            required: '必須項目です。',
+            integer: '数値で入力してください'
+        }
+    }
 };
