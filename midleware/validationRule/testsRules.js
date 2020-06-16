43faa91a4e9f282186b14b905a9feb71710c053e
Naoumi1214 <wada121400@gmail.com>
2020-06-16 14:06:08 +0900

    update : post用のルールを書いた

--- midleware/validationRule/testsRules.js
+++ midleware/validationRule/testsRules.js
@@ -1,11 +1,10 @@
-const {check, param} = require('express-validator');
-
 module.exports = {
     //post用
-    post: [check('text').not().isEmpty().withMessage('必須項目です。')],
+    post: {
+        text: 'required'
+    },
     //put用
     put: [
-        check('id').isNumeric().withMessage('数値を入力してください。'),
-        check('text').not().isEmpty().withMessage('必須項目です。')
+
     ]
 };
