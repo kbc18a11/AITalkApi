3afd39bd6c58cbfd7985806d01ad7dd7aba97be0
Naoumi1214 <wada121400@gmail.com>
2020-06-17 16:43:01 +0900

    update : deleteリクエストのサンプルの実装が完了した

--- routes/tests.js
+++ routes/tests.js
@@ -130,6 +130,18 @@ router.delete('/test/:id', async (req, res, next) => {
             }
         });
     }
+
+    try {
+        //レコードの削除開始
+        await Tests.delete(verificationValue.id);
+        return res.send({'updateResult': true});
+    } catch (error) {
+        //レコードの削除失敗時
+        console.log(error);
+        return res.status(500).send({'updateResult': false});
+    }
+
+
 })
 
 module.exports = router;
